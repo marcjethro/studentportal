1ef6c09bb075720207ce7ee0c43c4f4db7282268
marcjethro <marcjethro8@gmail.com>
2026-09-04 21:42:47 +0800

    Removed test

--- app/routes.py
+++ app/routes.py
@@ -3,13 +3,3 @@ from app import app
 @app.route('/')
 def homepage():
     return "Hello, This is Section 2's Student Portal!"
-
-data_store = [
-        {"id": 1, "name": "Alice", "role": "Engineer"},
-        {"id": 2, "name": "Bob", "role": "Designer"}
-        ]
-
-@app.route('/api/whatever')
-def whatever():
-    return data_store
-
