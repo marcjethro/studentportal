32a13ab8acb8c2dc532fa96280999caad1b51dfd
marcjethro <marcjethro8@gmail.com>
2026-09-04 21:25:05 +0800

    Testing JSON API

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,16 @@
 from app import app
+from flask import Flask, jsonify, request
 
 @app.route('/')
 def homepage():
     return "Hello, This is Section 2's Student Portal!"
+
+data_store = [
+        {"id": 1, "name": "Alice", "role": "Engineer"},
+        {"id": 2, "name": "Bob", "role": "Designer"}
+        ]
+
+@app.route('/api/whatever')
+def homepage():
+    return data_store
+
