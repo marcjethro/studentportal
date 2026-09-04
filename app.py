854284f0c34b9f35f7945254f437e27fe57bc9c5
marcjethro <marcjethro8@gmail.com>
2026-09-04 19:09:14 +0800

    Changed hello world

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def homepage():
+    return "Hello, This is Section 2's Student Portal!"
