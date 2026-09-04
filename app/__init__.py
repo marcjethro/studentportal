12e6958e028a5ea3b14a7b9fbe9a1ff751437ec5
marcjethro <marcjethro8@gmail.com>
2026-09-05 00:43:27 +0800

    Changed CORS Policy

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 db_uri = getenv("DB_URI")
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
 
 db = SQLAlchemy(app)
